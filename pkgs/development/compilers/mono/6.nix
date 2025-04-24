47ab8c0bf818c2d805917d2162559928e8e07ea0
Grimmauld <Grimmauld@grimmauld.de>
2025-04-24 13:08:45 +0200

    mono: 6.12.0.182 -> 6.14.0
    The original Mono project [1] was archived.
    It is now maintained by WineHQ on their gitlab [2].
    
    This changes the homepage for the new mono package,
    and moves the source fetchers to the version profiles.
    
    [1] https://www.mono-project.com/
    [2] https://gitlab.winehq.org/mono/mono

--- pkgs/development/compilers/mono/6.nix
+++ pkgs/development/compilers/mono/6.nix
@@ -1,13 +1,13 @@
 {
   callPackage,
-  Foundation,
-  libobjc,
+  fetchurl,
 }:
 
-callPackage ./generic.nix ({
-  inherit Foundation libobjc;
-  version = "6.12.0.182";
-  srcArchiveSuffix = "tar.xz";
-  sha256 = "sha256-VzZqarTztezxEdSFSAMWFbOhANuHxnn8AG6Mik79lCQ=";
+callPackage ./generic.nix (rec {
+  version = "6.14.0";
   enableParallelBuilding = true;
+  src = fetchurl {
+    url = "https://dl.winehq.org/mono/sources/mono/mono-${version}.tar.xz";
+    hash = "sha256-bdZLOQD15dX1UBbYnM92NchznLszzbgcHDthYi6R1RA=";
+  };
 })
