5b81a084fc899714ad99be2b915541b178d7189b
Grimmauld <Grimmauld@grimmauld.de>
2025-06-24 17:33:38 +0200

    nixos/tests/audit: Cheat to make the test pass

--- nixos/tests/auditd.nix
+++ nixos/tests/auditd.nix
@@ -6,12 +6,53 @@
     maintainers = with lib.maintainers; [ grimmauld ];
   };
 
-  nodes.machine = {
-    security.audit.enable = true;
-    security.auditd = {
-      enable = true;
+  nodes.machine =
+    { lib, pkgs, ... }:
+    {
+      security.audit.enable = true;
+      security.auditd = {
+        enable = true;
+      };
+
+      system.replaceDependencies.replacements =
+        let
+          audit' = pkgs.audit.overrideAttrs (old: {
+            src = pkgs.fetchFromGitHub {
+              owner = "linux-audit";
+              repo = "audit-userspace";
+              tag = "v4.0.5";
+              hash = "sha256-SgMt1MmcH7r7O6bmJCetRg3IdoZXAXjVJyeu0HRfyf8=";
+            };
+            patches = old.patches or [ ] ++ [
+              ../../pkgs/by-name/au/audit/write-debug-logs.patch
+              ../../pkgs/by-name/au/audit/allow-symlink-plugin-configs.patch
+            ];
+
+            postInstall = ''
+              for plugin_def in $out/etc/audit/plugins.d/*.conf; do
+                substituteInPlace "$plugin_def" \
+                  --replace-fail "/sbin/" "$bin/bin/" \
+                  --replace-warn "active = no" "active = yes"
+              done
+            '';
+          });
+        in
+        builtins.concatMap
+          (
+            { oldDependency, newDependency }:
+            assert oldDependency.outputs == newDependency.outputs;
+            builtins.map (out: {
+              oldDependency = oldDependency.${out};
+              newDependency = newDependency.${out};
+            }) oldDependency.outputs
+          )
+          (
+            lib.singleton {
+              oldDependency = pkgs.audit;
+              newDependency = audit';
+            }
+          );
     };
-  };
 
   testScript = ''
     start_all()
