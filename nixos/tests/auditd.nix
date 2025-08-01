6477940ca6fecb3538965c1c166b510503da28ec
Grimmauld <Grimmauld@grimmauld.de>
2025-08-01 16:00:52 +0200

    nixos/tests/audit: update for runstatedir changes

--- nixos/tests/auditd.nix
+++ nixos/tests/auditd.nix
@@ -9,7 +9,6 @@
   nodes.machine =
     { pkgs, ... }:
     {
-      security.audit.enable = true;
       security.audit.backlogLimit = 8192;
 
       # https://github.com/linux-audit/audit-testsuite/blob/5a10451642ac1ba2fa4b31c06a21cf9aa2d38b66/tests/amcast_joinpart/test#L86
@@ -24,9 +23,19 @@
         # plugins.filter.active = true; # needs configuring allowlist/denylist
       };
 
+      security.audit = {
+        enable = true;
+        rules = [
+          "-a always,exit -F exe=${lib.getExe pkgs.hello} -k nixos-test"
+        ];
+      };
+
       security.polkit.enable = true;
 
-      environment.systemPackages = [ pkgs.audit-testsuite.runner ];
+      environment.systemPackages = [
+        pkgs.audit-testsuite.runner
+        pkgs.hello
+      ];
     };
 
   testScript = ''
@@ -34,15 +43,25 @@
     machine.wait_for_unit("audit-rules.service")
     machine.wait_for_unit("auditd.service")
     machine.wait_for_unit("network.target") # netfilter test requires network
-    machine.succeed("stat /var/run/audispd_events")
-    machine.succeed("stat /var/log/audit/audit.log")
-    machine.succeed("stat /etc/audit/auditd.conf")
 
-    # we need a valid session to which we can send commands, so we use run0
-    machine.succeed("run0 --pty audit-testsuite-runner")
+    with subtest("Audit subsystem gets enabled"):
+      assert "enabled 1" in machine.succeed("auditctl -s")
 
-    assert "enabled 1" in machine.succeed("auditctl -s")
-    machine.succeed("systemctl stop audit-rules.service")
-    assert "enabled 0" in machine.succeed("auditctl -s")
+    with subtest("Custom rule produces audit traces"):
+      machine.succeed("hello")
+      print(machine.succeed("ausearch -k nixos-test -sc exit_group"))
+
+    with subtest("audit files are present"):
+      machine.succeed("stat /run/audit/audispd_events")
+      machine.succeed("stat /var/log/audit/audit.log")
+      machine.succeed("stat /etc/audit/auditd.conf")
+
+    with subtest("audit-testsuite"):
+      # we need a valid session to which we can send commands, so we use run0
+      machine.succeed("run0 --pty audit-testsuite-runner")
+
+    with subtest("Stopping audit-rules.service disables the audit subsystem"):
+      machine.succeed("systemctl stop audit-rules.service")
+      assert "enabled 0" in machine.succeed("auditctl -s")
   '';
 }
